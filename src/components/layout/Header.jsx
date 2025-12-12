36352c17d0951650312ddc89ec291342b1659f82
Faran khan <faran7067@gmail.com>
2025-12-12 12:33:02 +0500

    Update Header.jsx

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,7 +1,7 @@
 export default function Header() {
   return (
     <div className="h-16 bg-white shadow flex items-center justify-end px-6 mb-4">
-      <span className="text-gray-600 font-medium">Welcome 👋</span>
+      <span className="text-gray-600 font-medium">Welcome </span>
     </div>
   );
 }
